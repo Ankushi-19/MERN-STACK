398a7631ff8ad4d82e7d0ab38126121062cb12dd
Ankushi-19 <ankushidutta2001@gmail.com>
2024-06-26 10:20:48 +0530

    Assignment2

---
+++ assignment2/a2_q3.js
@@ -0,0 +1,7 @@
+//3. Explain the output of the following code snippet and why,
+console.log(foo);
+var foo="hello world";
+console.log(foo);
+
+//answer:
+//we can change a defined variable using var datatype because js is a loosly type language,but we can't change if we use const.
