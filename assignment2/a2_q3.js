19ac9286c1a191f7ee9492ce3cbe4a7ca624c39a
Ankushi Dutta <152770107+Ankushi-19@users.noreply.github.com>
2024-06-26 09:59:08 +0530

    Delete assignment2 directory

--- assignment2/a2_q3.js
+++
@@ -1,7 +0,0 @@
-//3. Explain the output of the following code snippet and why,
-console.log(foo);
-var foo="hello world";
-console.log(foo);
-
-//answer:
-//we can change a defined variable using var datatype because js is a loosly type language,but we can't change if we use const.
--- assignment2/a2_q4.js
+++
@@ -1,6 +0,0 @@
-// 4. Merge two arrays and add an additional element at the beginning of the merged array using the spread operator.
-
-let a=[2,4,6,0,8];
-let b=[3,5,8,1,7];
-let newarr=([24,...a,...b]);
-console.log(JSON.stringify(newarr));
